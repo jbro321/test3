61c700ba3206c0c4c16fb99324f7c8f284a2cae4
JP <jaehyung0321@gmail.com>
2021-07-14 02:47:27 +0900

    test3

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 
 def index(request):
+    # pybo 목록 출력
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
     return HttpResponse("안녕하세요 pybo입니다. 반갑습니다.")
