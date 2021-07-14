88e509bf6492c160ff0cd2f7b03b754c1cbc5493
JP <jaehyung0321@gmail.com>
2021-07-15 05:28:16 +0900

    test3

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Question
 
@@ -8,4 +8,11 @@ def index(request):
     # pybo 목록 출력
     question_list = Question.objects.order_by('-create_date')
     context = {'question_list': question_list}
-    return HttpResponse("안녕하세요 pybo입니다. 반갑습니다.")
+    return render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+    # pybo 내용 출력
+    # question = Question.objects.get(id=question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
