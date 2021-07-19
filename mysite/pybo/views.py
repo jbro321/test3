7de0ee08f0d929904edbe1c87139250104ef2dde
JP <jaehyung0321@gmail.com>
2021-07-19 14:18:56 +0900

    update views

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -29,9 +29,16 @@ def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     if request.method == "POST":
         form = AnswerForm(request.POST)
-
-    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
-    return redirect('pybo:detail', question_id=question.id)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        form = AnswerForm()
+    context = {'question': question, 'form': form}
+    return render(request, 'pybo/question_detail.html', context)
 
 def question_create(request):
     """
